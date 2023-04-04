025ba5a35f97666600e2d27583037394531b2a91
Ajay Sarkate <ajusarkate2002@gmail.com>
2023-04-04 14:46:04 +0530

    first

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ btn.addEventListener("click", getImageOfTheDay);
 
 function getImageOfTheDay(){
     let newDate = searchDate.value;
-    alert(newDate);
+    // alert(newDate);
     fetch(`https://api.nasa.gov/planetary/apod?api_key=lif0dYtK9xpLwnIBOu9WL80rFT0Pn5oeVremXK2W&date=${newDate}`)
         .then((res) => {
             return res.json();
@@ -43,34 +43,37 @@ function getImageOfTheDay(){
         })
     
     saveSearch(newDate);
-    // addSearchToHistory(newDate);
+    addSearchToHistory(newDate);
 }
 let searchResult = document.querySelector("#search-result");
+
+
 function saveSearch(newDate){
     arr.push(newDate);
     localStorage.setItem("searches", JSON.stringify(arr));
-
-    // alert(newDate);
-    searchResult.innerHTML += `<h4 ><a onclick="getUserDate(${newDate})">${newDate}</a> <h4>`;
-    // addSearchToHistory(newDate);
 }
 
 
-// function addSearchToHistory(newDate){
-//     // let searchHistory = JSON.parse(localStorage.getItem("searches"));
-//     console.log();
+function addSearchToHistory(newDate){
+    let searchHistory = JSON.parse(localStorage.getItem("searches"));
+    
+    const myHtml = searchHistory.map((item) => {
+        const temp = item.split("-");
+        return`<li><a href="#" onclick="getUserDate(${temp[0]}, ${temp[1]} ,${temp[2]})">${temp[0]}-${temp[1]}-${temp[2]}</a></li>`;
+    })
 
-//     searchResult.innerHTML += `<h4 ><a onclick="getUserDate(${newDate})">${newDate}</a> <h4>`;
-// }
+    searchResult.innerHTML = myHtml.join(" ");
+}
 
-function getUserDate(newDate){
-    alert(newDate+2.1);
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=lif0dYtK9xpLwnIBOu9WL80rFT0Pn5oeVremXK2W&date=${newDate+1}`)
+
+function getUserDate(year, month,date){
+    console.log(year,month,date);
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=dmylZQirmdOEA2EO1mdEuEl3tnTjfffH9I8Ccj4Q&date=${year}-${month}-${date}`)
         .then((res) => {
             return res.json();
         })
         .then((data) => {
-            console.log(data);
+            console.log("udiwui",data);
             title.innerHTML = `<h1>Picture On ${data.date} </h1>`;
             img1.innerHTML = `<img src="${data.hdurl}">`;
             pictureDetails.innerHTML = `<h3>${data.title} </h3> <p> ${data.explanation} </p>`;
